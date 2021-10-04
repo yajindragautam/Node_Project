ae884217b63edfeb6cc22f9ff7b3503e2bb13759
Yajindra <yajindragtm@gmail.com>
2021-10-04 19:47:37 +0545

    CROD ON ARRAY

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 
-app.get('/',(req,res)=>{
+// Express Routes
+/*app.get('/',(req,res)=>{
   res.json({
     message: 'Hello World'
   })
@@ -9,7 +11,29 @@ app.get('/',(req,res)=>{
 
 app.get('about',(req,res)=>{
   res.send('This is about our history')
+}) */
+
+// CROU IN ARRAY
+let users = [];
+
+// get all users, Request Method: Get
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// create new users, Request Method: POST
+app.post("/users", (req, res) => {
+  req.json(req)
+});
+
+// get user by id, Request Method: Get
+app.get('/users/:id',(req, res) => {
+  let user = users.find(user => user.id === parseInt(req.params.id));
+  if (!user) res.status(404).send('The user with the given ID was not found.');
+  res.json(user);
 })
 
-app.listen(3000);
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
