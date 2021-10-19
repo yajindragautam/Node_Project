bb7725bbaff4e349071f3fa8743138e1a88ad82a
Yajindra <yajindragtm@gmail.com>
2021-10-19 10:11:56 +0545

    Managed files

--- index.js
+++ index.js
@@ -1,69 +1,23 @@
 require("dotenv").config();
-const bodyParser = require("body-parser");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const route = require("./routes/index");
 
-// Express Routes
-/*app.get('/',(req,res)=>{
-  res.json({
-    message: 'Hello World'
-  })
-})
+app.use(bodyParser.json());
 
-app.get('about',(req,res)=>{
-  res.send('This is about our history')
-}) */
-
-// CROU IN ARRAY
-let users = [];
-
-// get all users, Request Method: Get
-app.get("/users", (req, res) => {
-  res.json(users);
+//middleware
+app.use((req, res, next) => {
+  console.log("This is middleware! Body: ", req.body);
+  // res.json({message: 'returned from middleware!'})
+  next();
 });
 
-// create new users, Request Method: POST
-app.post("/users", (req, res) => {
-  users.push(req.body);
-  res.status(201).json(req.body);
-});
+app.use("/", route);
 
-// get user by id, Request Method: Get
-app.get('/users/:id',(req, res) => {
-  let user = users.find(user => user.id === parseInt(req.params.id));
-  if (!user) res.status(404).send('The user with the given ID was not found.');
-  res.json(user);
-})
-
-//
-app.put("/users/:id",(req,res)=>{
-  let userIndex = user.findIndex((user)=> user.id === parseInt(req.params.id));
-  if(userIndex > users.length){
-    return res.status(404).json({
-      error:'User Not Found'
-    })
-  }
-  users[userIndex]['name'] = req.body.name;
-  users[userIndex]["email"] = req.body.email;
-  users[userIndex]["address"] = req.body.address;
-  res.json(req.body);
-});
-
-// DELETE
-app.delete('/users/:id',(req,res)=>{
-  let userIndex = user.findIndex((user) => user.id === parseInt(req.params.id));
-  if (userIndex > users.length) {
-    return res.status(404).json({
-      error: "User Not Found",
-    });
-  }
-
-  users.splice(userIndex,1);
-  res.status(204).json({message:"The user has been deleted"});
-})
-
+//crud
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Listening to port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
---
+++ routes/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+
+// CROU IN ARRAY
+let users = [];
+
+// get all users, Request Method: Get
+router.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// create new users, Request Method: POST
+router.post("/users", (req, res) => {
+  users.push(req.body);
+  res.status(201).json(req.body);
+});
+
+// get user by id, Request Method: Get
+router.get("/users/:id", (req, res) => {
+  let user = users.find((user) => user.id === parseInt(req.params.id));
+  if (!user) res.status(404).send("The user with the given ID was not found.");
+  res.json(user);
+});
+
+//
+router.put("/users/:id", (req, res) => {
+  let userIndex = user.findIndex((user) => user.id === parseInt(req.params.id));
+  if (userIndex > users.length) {
+    return res.status(404).json({
+      error: "User Not Found",
+    });
+  }
+  users[userIndex]["name"] = req.body.name;
+  users[userIndex]["email"] = req.body.email;
+  users[userIndex]["address"] = req.body.address;
+  res.json(req.body);
+});
+
+// DELETE
+router.delete("/users/:id", (req, res) => {
+  let userIndex = user.findIndex((user) => user.id === parseInt(req.params.id));
+  if (userIndex > users.length) {
+    return res.status(404).json({
+      error: "User Not Found",
+    });
+  }
+
+  users.splice(userIndex, 1);
+  res.status(204).json({ message: "The user has been deleted" });
+});
+
+module.exports = router;
