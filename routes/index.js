4d8f69994ea29f82a616ab3a692a9b4c2daf292c
Yajindra <yajindragtm@gmail.com>
2021-10-19 12:05:33 +0545

    up

--- routes/index.js
+++ routes/index.js
@@ -18,19 +18,27 @@ router.post("/users", (req, res) => {
 // get user by id, Request Method: Get
 router.get("/users/:id", (req, res) => {
   let user = users.find((user) => user.id === parseInt(req.params.id));
-  if (!user) res.status(404).send("The user with the given ID was not found.");
+  if (!user) {
+    return res.status(404).json({
+      error: "The user with the given ID was not found.",
+    });
+  }
   res.json(user);
 });
 
 //
 router.put("/users/:id", (req, res) => {
-  let userIndex = user.findIndex((user) => user.id === parseInt(req.params.id));
+  let userIndex = users.findIndex(
+    (user) => user.id === parseInt(req.params.id)
+  );
   if (userIndex > users.length) {
     return res.status(404).json({
       error: "User Not Found",
     });
   }
+  //users[userIndex] = req.body;   -- Update whole body
   users[userIndex]["name"] = req.body.name;
+  users[userIndex]["age"] = req.body.age;
   users[userIndex]["email"] = req.body.email;
   users[userIndex]["address"] = req.body.address;
   res.json(req.body);
@@ -44,7 +52,6 @@ router.delete("/users/:id", (req, res) => {
       error: "User Not Found",
     });
   }
-
   users.splice(userIndex, 1);
   res.status(204).json({ message: "The user has been deleted" });
 });
